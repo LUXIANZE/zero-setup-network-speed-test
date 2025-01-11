76ba8feea48349559b5c3a96c674c3066f1805c2
Xianze, Lu <luxianze98@gmail.com>
2025-01-11 16:44:00 +0800

    Add error logging

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ import SpeedTest from '@cloudflare/speedtest';
 const message: string = 'Hello from TypeScript and Webpack!';
 console.log(message);
 
-new SpeedTest().onFinish = results => console.log(results.getSummary());
+const speedTest = new SpeedTest();
+speedTest.onFinish = results => console.log(results.getSummary());
+speedTest.onError = error => console.error(error);
